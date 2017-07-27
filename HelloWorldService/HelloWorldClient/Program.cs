0a63f736d6c0b765357faa93a48933c4f6bca2df
Rob Watts <sleepnever@gmail.com>
2017-07-26 20:53:07 -0700

    add Client project

---
+++ HelloWorldService/HelloWorldClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5794/api/contacts/"); // DO NOT FORGET trailing slash
+
+            HttpResponseMessage result = client.GetAsync("contacts").Result;
+            string jsonResult = result.Content.ReadAsStringAsync().Result; // .Result says give it to me NOW
+
+
+        }
+    }
+}
